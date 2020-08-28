350e139ea55cc64a21395cf2e9039a941829c1e5
amay428 <amayagarwal428@gmail.com>
2020-08-28 23:55:56 +0530

    File commented and extra photos deleted

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
-import random
-import pygame
-import math
-from time import sleep
+#Import necessary modules
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" #Hide the starting intialize text
+import random #Get random values
+import pygame #Module for creating games
+import math #For applying math
+import time #To measure time
 
-# Intialize the pygame
+# Intialize pygame
 pygame.init()
 
 # create the screen
@@ -11,26 +14,21 @@ screen = pygame.display.set_mode((480, 480))
 
 # Background
 background = pygame.image.load('floor.png')
-coin = pygame.image.load('coin2.jpg')
+#Uranium
+uranium = pygame.image.load('uranium.png')
 
+#Define color codes in RGB for text background and colour
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-mode = 0
-
+#Define font size and write out the rules
 font = pygame.font.Font('freesansbold.ttf', 11)
-text1 = font.render('You have made it into the soviet factory. You are in the entrance room of the factory.', True, black, white)
-text2 = font.render('There is a hallway leading into the main room of the factory. ', True, black, white)
-text3 = font.render('You hold nothing but a silenced pistol.And of course, some general tools that ', True, black, white)
-text4 = font.render('can help on a stealth mission like this where you have to lurk ', True, black, white)
-
-
-
-num = 0
+text1 = font.render('CONTROLS: ARROW KEYS TO MOVE AND SPACE TO SHOOT', True, black, white)
+text2 = font.render('OBJECTIVE: GET THE URANIUM AT THE END OF THE HALL', True, black, white)
 
 pygame.display.set_caption("Perspectives") #Game title
-icon = pygame.image.load('hero.png') #Imgae icon
-pygame.display.set_icon(icon)
+icon = pygame.image.load('hero.png') #Image icon
+pygame.display.set_icon(icon) #Set icon
 
 playerImg = pygame.image.load('hero.png') #Load in the image of the player
 playerX = 400 #Player starting coordinates
@@ -44,8 +42,9 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_of_enemies = 6
+num_of_enemies = 20
 
+#Load in the bullet
 bulletImg = pygame.image.load('bullet.png')
 bulletX = 0
 bulletY = 480
@@ -60,7 +59,6 @@ for i in range(num_of_enemies): #Load number of enemies in a for loop
     enemyX_change.append(1)
     enemyY_change.append(20)
 
-wall = pygame.image.load('wall2.png') #Load image of wall behind which hero hides
 
 def player(x, y): #define function to place hero in certain coordinates
     screen.blit(playerImg, (x, y)) #scree.blit places image on the screen
@@ -69,18 +67,28 @@ def player(x, y): #define function to place hero in certain coordinates
 def enemy(x, y, i): #define function to place enemy in certain coordinates
     screen.blit(enemyImg[i], (x, y))
 
-def fire_bullet(x, y):
+def fire_bullet(x, y): #Define a function to fire the bullet
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletImg, (x + 16, y + 10))
 
 
-def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+def isCollision(enemyX, enemyY, bulletX, bulletY): #define a function to check for collision
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2))) #Formula for distance
     if distance < 27:
         return True
     else:
         return False
+
+def game_over_text(): #Show losin text
+    over_font = pygame.font.Font('freesansbold.ttf', 25)
+    over_text = over_font.render("A SOVIET SOLDIER KILLED YOU", True, (255, 0, 0))
+    screen.blit(over_text, (35, 250))
+
+def you_won_text(): #Show winning text
+    won_font = pygame.font.Font('freesansbold.ttf', 25)
+    won_text = won_font.render("You won!", True, (255, 0, 0))
+    screen.blit(won_text, (35, 250))
 
 # Game Loop
 running = True
@@ -90,7 +98,9 @@ while running:
     screen.fill((0, 0, 0)) #Fill the screen with black
     # Background Image
     screen.blit(background, (0, 0)) #add background layer
-    screen.blit(wall, (360, 370)) #add wall
+    screen.blit(text1, (0, 450)) #Add the text
+    screen.blit(text2, (0, 460))
+    screen.blit(uranium, (240, 20)) #Add the uranium
 
     for event in pygame.event.get(): #if user presses x then quit
         if event.type == pygame.QUIT:
@@ -107,7 +117,7 @@ while running:
             if event.key == pygame.K_DOWN:
                 playerY_change = 1 #Change the value of playerY_change
             if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
+                if bullet_state == "ready":
                     # Get the current x cordinate of the player
                     bulletX = playerX
                     fire_bullet(bulletX, bulletY)
@@ -147,10 +157,12 @@ while running:
                 enemyX_change.append(1)
                 enemyY_change.append(20)
 
-        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY) #Use the collision method
         if collision:
             bulletY = 480
             bullet_state = "ready"
+
+            #Kill an enemy if it is shot
             del enemyX[i]
             del enemyY[i]
             del enemyX_change[i]
@@ -159,21 +171,43 @@ while running:
             break
 
 
-        enemyX[i] += enemyX_change[i]
+        enemyX[i] += enemyX_change[i] #Defines a boundary for the enemies
         if enemyX[i] <= 0:
-            enemyX_change[i] = 1
+            enemyX_change[i] = 3
             enemyY[i] += enemyY_change[i]
         elif enemyX[i] >= 459:
-            enemyX_change[i] = -1
+            enemyX_change[i] = -3
             enemyY[i] += enemyY_change[i]
 
-        enemy(enemyX[i], enemyY[i], i)
+        if playerX <= enemyX[i]-3: #If a player comes near a soldier, then the soldier kills it
+            if playerY <= 17:
+                game_over_text()
+                print("You lost")
+                quit()
+            else:
+                pass
+        else:
+            pass
 
-    if bulletY <= 0:
+        if playerX <= 240-3: #If a player is near the uranium, say tht they won
+            if playerY <= 20-3:
+                you_won_text()
+                print("You won!")
+                quit()
+            else:
+                pass
+        else:
+            pass
+
+                
+
+        enemy(enemyX[i], enemyY[i], i) #Teleport enemy to position
+
+    if bulletY <= 0: #Fire bullets
         bulletY = 480
         bullet_state = "ready"
 
-    if bullet_state is "fire":
+    if bullet_state == "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
